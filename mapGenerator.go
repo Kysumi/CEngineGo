247342bb3ae3cf31dfef08996be4ccffdecf07cb
Maunder <maunderscott@gmail.com>
2019-06-12 20:59:27 +1200

    Added asset and another tile.

--- mapGenerator.go
+++ mapGenerator.go
@@ -24,7 +24,7 @@ func getTiles() map[string][]TileDetails {
 
 	tilesSets["grass"] = append(
 		tilesSets["grass"],
-		TileDetails{"img_grass_0.png", 20, true, 0})
+		TileDetails{"img_grass_0.png", 150, true, 0})
 
 	tilesSets["grass"] = append(
 		tilesSets["grass"],
@@ -32,15 +32,31 @@ func getTiles() map[string][]TileDetails {
 
 	tilesSets["grass"] = append(
 		tilesSets["grass"],
-		TileDetails{"img_grass_2.png", 60, true, 0})
+		TileDetails{"img_grass_2.png", 200, true, 0})
 
 	tilesSets["grass"] = append(
 		tilesSets["grass"],
-		TileDetails{"img_grass_3.png", 20, true, 0})
+		TileDetails{"img_grass_3.png", 100, true, 0})
+
+	tilesSets["grass"] = append(
+		tilesSets["grass"],
+		TileDetails{"img_dirt_0.png", 150, true, 0})
 
 	tilesSets["grass"] = append(
 		tilesSets["grass"],
 		TileDetails{"img_grass_4.png", 300, true, 0})
+
+	tilesSets["barron"] = append(
+		tilesSets["barron"],
+		TileDetails{"img_barron_0.png", 80, true, 0})
+
+	tilesSets["sand"] = append(
+		tilesSets["sand"],
+		TileDetails{"img_sand_0.png", 200, true, 0})
+
+	tilesSets["stone"] = append(
+		tilesSets["stone"],
+		TileDetails{"img_stone_0.png", 30, true, 0})
 
 	return tilesSets
 }
@@ -50,27 +66,31 @@ func generate(newMap *Map) {
 
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	for len(tiles["grass"]) > 0 {
-		position := randomPosition(newMap.size)
-		neighbours := newMap.getNeighbourTiles(position, true)
+	for key := range tiles {
+		for len(tiles[key]) > 0 {
+			position := randomPosition(newMap.size)
+			neighbours := newMap.getNeighbourTiles(position, true)
 
-		for _, neighbour := range neighbours {
+			for _, neighbour := range neighbours {
+				if rand.Intn(10) > 2 {
+					continue
+				}
+				tileOptions := len(tiles[key])
 
-			tileOptions := len(tiles["grass"])
+				if tileOptions <= 0 {
+					break
+				}
 
-			if tileOptions <= 0 {
-				break
-			}
+				arrayLoc := r.Intn(tileOptions)
+				currentTileDetails := &tiles[key][arrayLoc]
 
-			arrayLoc := r.Intn(tileOptions)
-			currentTileDetails := &tiles["grass"][arrayLoc]
+				neighbour.SetSprite(loadSprite(currentTileDetails.spriteName))
 
-			neighbour.SetSprite(loadSprite(currentTileDetails.spriteName))
+				currentTileDetails.consumed += 1
 
-			currentTileDetails.consumed += 1
-
-			if currentTileDetails.consumed >= currentTileDetails.maxTileCount {
-				tiles["grass"] = append(tiles["grass"][:arrayLoc], tiles["grass"][arrayLoc+1:]...)
+				if currentTileDetails.consumed >= currentTileDetails.maxTileCount {
+					tiles[key] = append(tiles[key][:arrayLoc], tiles[key][arrayLoc+1:]...)
+				}
 			}
 		}
 	}
