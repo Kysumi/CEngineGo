fc141f5dd2a3af7aad67368f0be983e9b3643009
Maunder <maunderscott@gmail.com>
2019-08-07 20:30:50 +1200

    auto formatting

--- TileDetailsConway.go
+++ TileDetailsConway.go
@@ -1,11 +1,11 @@
 package main
 
 type TileDetailsConway struct {
-	DeathTileType int `json:"death_tile_type"`
-	UnderPop     int `json:"under_pop"`
-	OverPop      int `json:"over_pop"`
-	Reproduction int `json:"reproduction"`
-	Death        int `json:"death"`
-	Tile   string `json:"tile"`
-	NeighboursReach   int `json:"neighbours_reach"`
+	DeathTileType   int    `json:"death_tile_type"`
+	UnderPop        int    `json:"under_pop"`
+	OverPop         int    `json:"over_pop"`
+	Reproduction    int    `json:"reproduction"`
+	Death           int    `json:"death"`
+	Tile            string `json:"tile"`
+	NeighboursReach int    `json:"neighbours_reach"`
 }
